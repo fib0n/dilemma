a87b6a2e8228293fda055cc348ee279e464f1dc8
Andrey Ivanov <ans.ivanov@gmail.com>
2015-02-15 16:09:43 +0300

    added tests

--- README.md
+++ README.md
@@ -2,4 +2,16 @@ Prisoner's Dilemma
 ========
 Моделирование поведения команды игроками с разными стратегиями
 
-Для просмотра результатов и изменения параметров моделирования необходимо скопировать все файлы и открыть index.html в современном браузере (желательно в chrome - разработка и тестирование проводилась в версии chrome 40.0.2214.94 (64-bit))
+Для просмотра результатов и изменения параметров моделирования необходимо скопировать все файлы и открыть [index.html](https://github.com/fib0n/dilemma/blob/master/index.html) в современном браузере (желательно в chrome - разработка и тестирование проводилась в версии chrome 40.0.2214.94 (64-bit))
+
+Usage:
+==
+```
+git clone https://github.com/fib0n/dilemma.git && cd dilemma
+npm install
+npm test
+```
+
+Для тестирования используется [Jasmine](http://jasmine.github.io/). Покрытие кода считается с помощью [Istanbul](https://github.com/gotwarlost/istanbul) и [Karma](https://github.com/karma-runner/karma)
+
+Тестирование и анализ покрытия проводился для файла [strategy.js](https://github.com/fib0n/dilemma/blob/master/js/strategy.js). Сгенерированные отчеты о результатах теста лежат в [jasmine.html](https://github.com/fib0n/dilemma/blob/master/js/strategy.spec/jasmine.html). Сгенерированный отчет о покрытии кода [index.html](https://github.com/fib0n/dilemma/blob/master/js/coverage/PhantomJS%201.9.8%20(Mac%20OS%20X)/index.html).
--- js/strategy.js
+++ js/strategy.js
@@ -1,52 +1,52 @@
-window.app = window.app || {};
-(function f() {
+//var window = window || {};
+(function f(window, undefined) {
     'use strict';
-    var app = window.app,
-        DEFECT = window.app.DEFECT || 1,
-        COOPERATE = window.app.COOPERATE || 0;
+    var app = window.app = window.app || {},
+        DEFECT = app.DEFECT = window.app.DEFECT || 1,
+        COOPERATE = app.COOPERATE = window.app.COOPERATE || 0;
 
-    function AllC() {
+    app.AllC = function AllC() {
         return COOPERATE;
     }
 
-    function AllD() {
+    app.AllD = function AllD() {
         return DEFECT;
     }
 
-    function Rand() {
+    app.Rand = function Rand() {
         return (Math.random() > 0.5) ? COOPERATE : DEFECT;
     }
 
-    function Grim(opponentHistory) {
+    app.Grim = function Grim(opponentHistory) {
         return opponentHistory.some(function(elem) {
             return elem.move === DEFECT;
         }) ? DEFECT : COOPERATE;
     }
 
-    function Neg(opponentHistory) {
+    app.Neg = function Neg(opponentHistory) {
         var lastOpponent = opponentHistory.slice(-1)[0];
         return (lastOpponent && lastOpponent.move === COOPERATE) ? DEFECT : COOPERATE;
     }
 
-    function TFT(opponentHistory) {
+    app.TFT = function TFT(opponentHistory) {
         var lastOpponent = opponentHistory.slice(-1)[0];
         if (lastOpponent)
             return lastOpponent.move;
         return COOPERATE;
     }
 
-    function STFT(opponentHistory) {
+    app.STFT = function STFT(opponentHistory) {
         var lastOpponent = opponentHistory.slice(-1)[0];
         if (lastOpponent)
             return lastOpponent.move;
         return DEFECT;
     }
 
-    function TFTT(opponentHistory) {
+    app.TFTT = function TFTT(opponentHistory) {
         return opponentHistory.length > 1 && opponentHistory[opponentHistory.length - 1].move === DEFECT && opponentHistory[opponentHistory.length - 2].move === DEFECT ? DEFECT : COOPERATE;
     }
 
-    function Pavlov(opponentHistory, ownHistory) {
+    app.Pavlov = function Pavlov(opponentHistory, ownHistory) {
         if (ownHistory.length) {
             var lastOwn = ownHistory.slice(-1)[0];
             return lastOwn.success ? lastOwn.move : lastOwn.move === DEFECT ? COOPERATE : DEFECT;
@@ -54,5 +54,5 @@ window.app = window.app || {};
         return (Math.random() > 0.5) ? COOPERATE : DEFECT;
     }
 
-    app.strategies = [AllC, AllD, Rand, Grim, Neg, TFT, STFT, TFTT, Pavlov];
-}());
+    app.strategies = [app.AllC, app.AllD, app.Rand, app.Grim, app.Neg, app.TFT, app.STFT, app.TFTT, app.Pavlov];
+}(window, undefined));
